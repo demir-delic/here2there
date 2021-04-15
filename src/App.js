d622e55f86796a616d30a0c0b0fe1e51d1ec14b9
Demir Delic <demir.delic@gmail.com>
2021-04-15 21:20:54 +0200

    fix: correct spacing and bg-color, reduce number of custom styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from "./Button";
 import Header from "./Header";
 import LinkComponent from "./Link";
 import Modal from "./Modal";
-import Card from "./Cards";
+import Cards from "./Cards";
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import { useState } from "react";
 
@@ -19,31 +19,33 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/results">
-          <div className="fill-viewport bg-gray-50">
-            <Card></Card>
-            <div className="btn-container container mx-auto px-10">
+          <div className="h-min-screen bg-gray-50">
+            <Cards></Cards>
+            <div className="container w-max mx-auto mb-16">
               <Link to="/">
-                <Button text="Search Again" className="mb-20"></Button>
+                <Button text="Search Again"></Button>
               </Link>
             </div>
           </div>
         </Route>
         <Route path="/">
-          <div className="fill-viewport bg-gray-50">
+          <div className="h-min-screen bg-gray-50">
             <Modal open={openModal} onModalUpdate={setOpenModal} />
             {/* {`\nopenModal: ${openModal}`} */}
             <div className="cursor-pointer" onClick={toggleModal}>
               <LinkComponent></LinkComponent>
             </div>
             <Header userCity="Munich" userCountry="Germany"></Header>
-            <div className="form-control container mx-auto px-10">
+            <div className="container flex flex-col items-baseline justify-between w-max h-88 mx-auto px-10">
               <Switcher label="Less expensive" isEnabled={true}></Switcher>
               <Switcher label="Warmer in April" isEnabled={true}></Switcher>
               <Switcher label="Smaller population" isEnabled={true}></Switcher>
               <Switcher label="Safer" isEnabled={true}></Switcher>
               <Switcher label="Close to Munich" isEnabled={false}></Switcher>
               <Link to="/results">
-                <Button text="Find Vacation Spot" className="mt-10"></Button>
+                <div className="mt-6 mb-16">
+                  <Button text="Find Vacation Spot"></Button>
+                </div>
               </Link>
             </div>
           </div>
