adcd2a9f69cb48dacc78f96a88a9c9c7f46ca4d9
Demir Delic <demir.delic@gmail.com>
2021-04-18 23:16:36 +0200

    test: adjust sample test to see if landing page and results page render

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import SearchResultPage from "./SearchResultPage";
 
-test('renders learn react link', () => {
+test("renders landing page heading", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const landingPageHeading = screen.getByText(
+    /What should your next vacation destination be like compared to/i
+  );
+  expect(landingPageHeading).toBeInTheDocument();
+});
+
+test("renders results page heading", () => {
+  render(
+    <BrowserRouter>
+      <SearchResultPage />
+    </BrowserRouter>
+  );
+
+  const resultsPageHeading = screen.getByText(/Here are some places you might like to visit/i);
+  expect(resultsPageHeading).toBeInTheDocument();
 });
