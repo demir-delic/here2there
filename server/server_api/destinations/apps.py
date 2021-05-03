6dfdccb14f5203c2c637dc16cfc7988f44797e3f
Che <vee.langat@gmail.com>
2021-05-03 12:13:36 +0300

    ci: run pre-commit pipeline on server

--- server/server_api/destinations/apps.py
+++ server/server_api/destinations/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DestinationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'destinations'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "destinations"
