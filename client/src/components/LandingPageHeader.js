e3999242b4ca54b18a7e6e32ce91019666b1e89b
xarrijorge <kodinglife@gmail.com>
2021-05-07 20:13:26 +0000

    Formated files with prettier. Removed yarn.lock

--- client/src/components/LandingPageHeader.js
+++ client/src/components/LandingPageHeader.js
@@ -1,22 +1,21 @@
 export default function LandingPageHeader(props) {
-    let limiter = props.userCity !== null ? ',' : ''
-    return (
-        <div className='bg-gray-50'>
-            <div className='max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
-                <div className='text-center'>
-                    <p className='mt-5 mx-auto max-w-prose text-xl text-gray-500'>
-                        Looks like you're somewhere near
-                        <span className='font-semibold text-green-600'>
-                            {` ${props.userCity}${limiter} ${props.userCountry}`}
-                        </span>
-                    </p>
-                    <p className='max-w-xl mt-5 mx-auto text-2xl'>
-                        What should your next vacation destination be like
-                        compared to{' '}
-                        <span className='font-semibold text-green-600'>{` ${props.userCity}?`}</span>
-                    </p>
-                </div>
-            </div>
+  let limiter = props.userCity !== null ? "," : "";
+  return (
+    <div className="bg-gray-50">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500">
+            Looks like you're somewhere near
+            <span className="font-semibold text-green-600">
+              {` ${props.userCity}${limiter} ${props.userCountry}`}
+            </span>
+          </p>
+          <p className="max-w-xl mt-5 mx-auto text-2xl">
+            What should your next vacation destination be like compared to{" "}
+            <span className="font-semibold text-green-600">{` ${props.userCity}?`}</span>
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
