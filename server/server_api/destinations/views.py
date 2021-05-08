8492108154154f5530d5332d8437ec2cc68d8ae3
Che <vee.langat@gmail.com>
2021-05-08 14:16:08 +0300

    feat: endpoint that returns city with matching lat & long

--- server/server_api/destinations/views.py
+++ server/server_api/destinations/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
+
+from .models import Destination
+from .serializer import NearestCitySerializer
+
+
+class NearestCityViewSet(viewsets.ModelViewSet):
+    queryset = Destination.objects.all()
+    serializer_class = NearestCitySerializer
+
+    def get_queryset(self):
+
+        longitude = self.request.query_params.get("long")
+        latitude = self.request.query_params.get("lat")
+
+        if longitude is not None and latitude is not None:
+            queryset = self.queryset.filter(longitude=longitude, latitude=latitude)
+            return queryset
+        else:
+            raise ValidationError(
+                {
+                    "message": "One or more required parameters are missing. lat and long are required."
+                }
+            )
