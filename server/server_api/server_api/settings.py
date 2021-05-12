2b95f1b600a800e50c913f4b730b2db50479d2ae
Demir Delic <demir.delic@gmail.com>
2021-05-13 01:31:13 +0200

    build: add ipinfo.io to allowed hosts to try to fix cors error

--- server/server_api/server_api/settings.py
+++ server/server_api/server_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["here2there.herokuapp.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["here2there.herokuapp.com", "127.0.0.1", "localhost", "ipinfo.io"]
 
 # Application definition
 
